84a9ea222370cd7470c7cf4dc5bee0480b612acf
ethanluc7 <140726164+ethanluc7@users.noreply.github.com>
2023-09-17 17:30:31 -0400

    Update Details.js
    updated dated for htr 2023

--- src/sections/Details.js
+++ src/sections/Details.js
@@ -29,7 +29,7 @@ const Details = () => {
           /
         </Heading>
         <Heading fontSize={{ base: "0.5rem", sm: "xs", lg: "xl" }}>
-          December 10th, 2022
+          December 9th, 2023
         </Heading>
         <Heading
           fontSize={{ base: "0.5rem", sm: "xs", lg: "xl" }}
