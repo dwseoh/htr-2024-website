63d11142fa7b86e50e722ef7559a34502a993c9d
ethanluc7 <140726164+ethanluc7@users.noreply.github.com>
2023-10-08 18:20:51 -0400

    Update Navigation.js
    added logo and registration link

--- src/sections/Navigation.js
+++ src/sections/Navigation.js
@@ -5,12 +5,21 @@ import { Flex, HStack, Heading, Box } from "@chakra-ui/react";
 import { useColorMode } from "@chakra-ui/color-mode";
 import { IconButton } from "@chakra-ui/button";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import logo from "./img/logo.png";
 // import {} from "@chakra/icons"
 const Navigation = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
     <Flex w="full" mt={10} justifyContent="flex-end">
+        <Box pl={10}>
+          <img 
+          src = {logo.src} 
+          alt = 'logo'  
+          style = {{"object-fit": "cover", "width": "10%", "border-radius": "25px"}} 
+          />
+        </Box>
+                   
       <HStack spacing={59} pr={59} display={{ base: "none", lg: "flex" }}>
         <Heading as="a" href="#about" fontSize="lg">
           About
@@ -27,7 +36,7 @@ const Navigation = () => {
         <Heading as="a" href="#team" fontSize="lg">
           Team
         </Heading>
-        <Heading as="a" href="#register" fontSize="lg">
+        <Heading as="a" href="https://my.mlh.io/oauth/authorize?client_id=rvBQdXCDB9bGD1G0TlVwSwIRiG5_0oRjr8Zb4iIRd6o&redirect_uri=https%3A%2F%2Fhacktheridge.ca%2F&response_type=token" fontSize="lg">
           Register
         </Heading>
         <IconButton aria-label="Toggle Mode" onClick={toggleColorMode}>
