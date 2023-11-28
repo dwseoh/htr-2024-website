437dba3a038cd3f25f5cdab39d866787c9dd7f14
SD103 <120504958+SD103@users.noreply.github.com>
2023-11-27 23:04:50 -0500

    Update Sponsors.js
    Changed sponsors
    Maybe I should change the images for them.
    Also idk if the Catbox image urls actually embed
    
    I spent an extra 30 minutes on this because my WiFi kept cutting out -- I'm gonna go to sleep instead of checking if any of this worked

--- src/sections/Sponsors.js
+++ src/sections/Sponsors.js
@@ -40,60 +40,78 @@ const sponsors = [
   //   icon="https://static.mlh.io/brand-assets/logo/official/mlh-logo-color.png"
   //   link="https://mlh.io/"
   // />,
-  // <Sponsor
-  //   key="a"
-  //   name="Metro"
-  //   icon="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Metro_Inc._logo.svg/2560px-Metro_Inc._logo.svg.png"
-  //   link="https://www.metro.ca/en"
-  // />,
-  // <Sponsor
-  //   key="a"
-  //   name="replit"
-  //   icon="https://blog.replit.com/images/logo.png"
-  //   link="https://replit.com/"
-  // />,
-  // <Sponsor
-  //   key="a"
-  //   name="DQ"
-  //   icon="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Dairy_Queen_logo.svg/1200px-Dairy_Queen_logo.svg.png"
-  //   link="https://www.dairyqueen.com/en-ca/"
-  // />,
-  // <Sponsor
-  //   key="a"
-  //   name="Marble Slab Creamery"
-  //   icon="https://cdn.discordapp.com/attachments/933210665882091553/1046978763285614684/marble_slab_creamery_logo.png"
-  //   link="https://www.marbleslab.ca/"
-  // />,
-  // <Sponsor
-  //   key="a"
-  //   name="KFC"
-  //   icon="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/KFC_Logo.svg/2560px-KFC_Logo.svg.png"
-  //   link="https://www.kfc.ca/"
-  // />,
-  // <Sponsor
-  //   key="a"
-  //   name="Foodies curry & shawarma"
-  //   icon="https://i0.wp.com/foodiescns.ca/wp-content/uploads/2022/05/color-1.png?fit=500%2C132&ssl=1"
-  //   link="https://foodiescns.ca/"
-  // />,
-  // <Sponsor
-  //   key="a"
-  //   name="Harveys"
-  //   icon="https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Harvey%27s_logo.svg/1200px-Harvey%27s_logo.svg.png"
-  //   link="https://www.harveys.ca/en.html"
-  // />,
-  // <Sponsor
-  //   key="a"
-  //   name="Gotcha Bubble Tea"
-  //   icon="https://www.gotchafreshtea.com/uploads/1/2/3/7/123755766/gotcha-2018-logo-gotcha-eng_1.png"
-  //   link="https://gotcha-bubble-tea.business.site/"
-  // />,
-  // <Sponsor
-  //   key="a"
-  //   name="Code Club"
-  //   icon="https://cdn.discordapp.com/attachments/1005951424879931407/1050449083174764574/unknown-removebg-preview.png"
-  //   link="https://codeclub.org/en/"
-  // />,
+  <Sponsor
+    key="a"
+    name="Ontario Tech University"
+    icon="https://files.catbox.moe/ofd9zg.png"
+    link="https://ontariotechu.ca/"
+  />
+  <Sponsor
+    key="a"
+    name="Bruce Power"
+    icon="https://files.catbox.moe/iz51ys.png"
+    link="http://www.brucepower.com/"
+  />
+  <Sponsor
+    key="a"
+    name="Siemens"
+    icon="https://files.catbox.moe/des2np.png"
+    link="https://www.siemens.com/"
+  />
+  <Sponsor
+    key="a"
+    name="The University of Waterloo"
+    icon="https://files.catbox.moe/pnmpv4.png"
+    link="https://uwaterloo.ca/"
+  />
+  <Sponsor
+    key="a"
+    name="Evertz Microsystems"
+    icon="https://evertz.com/img/evertz/evertz.logo.svg"
+    link="https://evertz.com/"
+  />
+  <Sponsor
+    key="a"
+    name="Deloitte"
+    icon="https://www2.deloitte.com/content/dam/assets/logos/deloitte.svg"
+    link="https://www2.deloitte.com/ca/en/pages/about-deloitte/articles/home.html"
+  /> 
+  <Sponsor
+    key="a"
+    name="Interview Cake"
+    icon="https://assets-global.website-files.com/62e051559bac913234470995/63da686f8720100e177b4f40_Interview%20Cake%20main-p-800.webp"
+    link="https://www.interviewcake.com/"
+  /> 
+  <Sponsor
+    key="a"
+    name="McMaster University"
+    icon="https://files.catbox.moe/1p76xa.png"
+    link="https://www.mcmaster.ca/"
+  />
+  <Sponsor
+    key="a"
+    name="Weever Apps"
+    icon="https://files.catbox.moe/70lru2.png"
+    link="https://weeverapps.com/"
+  />  
+  <Sponsor
+    key="a"
+    name="Adapsyn"
+    icon="https://files.catbox.moe/z9xy49.png"
+    link="https://www.adapsyn.com/"
+  />  
+  <Sponsor
+    key="a"
+    name="Sheridan College"
+    icon="https://media-www.sheridancollege.ca/-/media/project/sheridan/shared/images/logos/sheridan-logo-header.svg?iar=0&rev=9aef8a944ba94fda83019bff6b668519&hash=CB36F99641395D235635808BC254DE63"
+    link="https://www.sheridancollege.ca/"
+  />   
+  <Sponsor
+    key="a"
+    name="Brock University"
+    icon="https://upload.wikimedia.org/wikipedia/en/7/7f/BrockU_CoA.jpg"
+    link="https://brocku.ca/"
+  />
 ].map((sponsor, i) => {
   return (
     <div className={`sponsor${i}`} key={i}>
