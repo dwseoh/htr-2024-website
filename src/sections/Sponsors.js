4491a7a04f15decb6dab709916966dff27eb237d
SD103 <120504958+SD103@users.noreply.github.com>
2023-11-28 23:12:51 -0500

    Update Sponsors.js
    Oopsies, forgot the commas

--- src/sections/Sponsors.js
+++ src/sections/Sponsors.js
@@ -45,67 +45,67 @@ const sponsors = [
     name="Ontario Tech University"
     icon="https://files.catbox.moe/ofd9zg.png"
     link="https://ontariotechu.ca/"
-  />
+  />,
   <Sponsor
     key="a"
     name="Bruce Power"
     icon="https://files.catbox.moe/iz51ys.png"
     link="http://www.brucepower.com/"
-  />
+  />,
   <Sponsor
     key="a"
     name="Siemens"
     icon="https://files.catbox.moe/des2np.png"
     link="https://www.siemens.com/"
-  />
+  />,
   <Sponsor
     key="a"
     name="The University of Waterloo"
     icon="https://files.catbox.moe/pnmpv4.png"
     link="https://uwaterloo.ca/"
-  />
+  />,
   <Sponsor
     key="a"
     name="Evertz Microsystems"
     icon="https://evertz.com/img/evertz/evertz.logo.svg"
     link="https://evertz.com/"
-  />
+  />,
   <Sponsor
     key="a"
     name="Deloitte"
     icon="https://www2.deloitte.com/content/dam/assets/logos/deloitte.svg"
     link="https://www2.deloitte.com/ca/en/pages/about-deloitte/articles/home.html"
-  /> 
+  />,
   <Sponsor
     key="a"
     name="Interview Cake"
     icon="https://assets-global.website-files.com/62e051559bac913234470995/63da686f8720100e177b4f40_Interview%20Cake%20main-p-800.webp"
     link="https://www.interviewcake.com/"
-  /> 
+  />,
   <Sponsor
     key="a"
     name="McMaster University"
     icon="https://files.catbox.moe/1p76xa.png"
     link="https://www.mcmaster.ca/"
-  />
+  />,
   <Sponsor
     key="a"
     name="Weever Apps"
     icon="https://files.catbox.moe/70lru2.png"
     link="https://weeverapps.com/"
-  />  
+  />,  
   <Sponsor
     key="a"
     name="Adapsyn"
     icon="https://files.catbox.moe/z9xy49.png"
     link="https://www.adapsyn.com/"
-  />  
+  />,  
   <Sponsor
     key="a"
     name="Sheridan College"
     icon="https://media-www.sheridancollege.ca/-/media/project/sheridan/shared/images/logos/sheridan-logo-header.svg?iar=0&rev=9aef8a944ba94fda83019bff6b668519&hash=CB36F99641395D235635808BC254DE63"
     link="https://www.sheridancollege.ca/"
-  />   
+  />,
   <Sponsor
     key="a"
     name="Brock University"
