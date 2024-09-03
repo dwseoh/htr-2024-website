5bcf9111c0fed4cd4e81d4b5b6d1e492e4a478cc
YellowLime77 (Matthew Yang) <matthewyanglele@gmail.com>
2024-09-03 15:48:56 -0400

    fix team pictures

--- src/sections/Team.js
+++ src/sections/Team.js
@@ -3,7 +3,18 @@ import { useColorModeValue } from "@chakra-ui/color-mode";
 import { Image, Img } from "@chakra-ui/image";
 import { Box, Flex, Heading, SimpleGrid, VStack } from "@chakra-ui/layout";
 import Thin from "./helpers";
-import shayaan from "./img/shayaan.jpg";
+
+import jamie from "./img/jamie.webp";
+import eric from "./img/eric.webp";
+import sebastian from "./img/sebastian.webp";
+import matthew from "./img/matthew.webp";
+import michelle from "./img/michelle.webp";
+import mythili from "./img/mythili.webp";
+import aahan from "./img/aahan.webp";
+import luqman from "./img/luqman.webp";
+import joshua from "./img/joshua.webp";
+import gavin from "./img/gavin.webp";
+import sid from "./img/sid.webp";
 
 import { useEffect } from "react";
 import gsap from "gsap";
@@ -44,47 +55,47 @@ function Member({ name, icon }) {
 const Members = [
   <Member
     name="Jamie Seoh"
-    icon="img/jamie.webp"
+    icon={jamie.src}
   />,
   <Member
     name="Eric Yang"
-    icon="img/eric.webp"
+    icon={eric.src}
   />,
   <Member
     name="Sebastian Barsan"
-    icon="img/sebastian.webp"
+    icon={sebastian.src}
   />,
   <Member
     name="Matthew Yang"
-    icon="img/matthew.webp"
+    icon={matthew.src}
   />,
   <Member
     name="Michelle Wang"
-    icon="img/michelle.webp"
+    icon={michelle.src}
   />,
   <Member
     name="Mythili Panicker"
-    icon="img/mythili.webp"
+    icon={mythili.src}
   />,
   <Member
     name="Aahan Ghode"
-    icon="img/aahan.webp"
+    icon={aahan.src}
   />,
   <Member
     name="Luqman Bhatti"
-    icon="img/luqman.webp"
+    icon={luqman.src}
   />,
   <Member
     name="Joshua Zhang"
-    icon="img/joshua.webp"
+    icon={joshua.src}
   />,
   <Member
     name="Gavin You"
-    icon="img/gavin.webp"
+    icon={gavin.src}
   />,
   <Member
     name="Sid Donaldson"
-    icon="img/sid.webp"
+    icon={sid.src}
   />,
 ].map((member, i) => {
   return (
