5095ec47622ea8a5718dfcafb8327d05a5fe42d2
TMZ-429 <120504958+TMZ-429@users.noreply.github.com>
2024-10-14 11:48:13 -0400

    Update Navigation.js

--- src/sections/Navigation.js
+++ src/sections/Navigation.js
@@ -29,7 +29,7 @@ const Navigation = () => {
         <Heading as="a" href="#team" fontSize="lg">
           Team
         </Heading>
-        <Heading as="a" href="https://forms.gle/sEGW2djeqhXsmd9J8" fontSize="lg">
+        <Heading as="a" href=links.registration fontSize="lg">
           Register
         </Heading>
         <IconButton aria-label="Toggle Mode" onClick={toggleColorMode}>
