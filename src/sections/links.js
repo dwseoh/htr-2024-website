1d87ce3b6894e6b20a08b5c267c0fbbdec6d89f5
TMZ-429 <120504958+TMZ-429@users.noreply.github.com>
2024-10-14 11:44:16 -0400

    Create links.js

---
+++ src/sections/links.js
@@ -0,0 +1,6 @@
+const links = {
+  "devpost": 'https://hack-the-ridge-2024.devpost.com/',
+  "registration": 'https://forms.gle/sEGW2djeqhXsmd9J8',
+  "instagram": "https://www.instagram.com/hacktheridge/",
+}
+export default links;
