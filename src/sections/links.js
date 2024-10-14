e2c3c2845c3200ab38adc9c4d3b04590dbfc7b71
TMZ-429 <120504958+TMZ-429@users.noreply.github.com>
2024-10-14 11:49:42 -0400

    Update links.js

--- src/sections/links.js
+++ src/sections/links.js
@@ -1,6 +1,7 @@
 const links = {
-  "devpost": 'https://hack-the-ridge-2024.devpost.com/',
-  "registration": 'https://forms.gle/sEGW2djeqhXsmd9J8',
-  "instagram": "https://www.instagram.com/hacktheridge/",
+  "devpost"     : "https://hack-the-ridge-2024.devpost.com/",
+  "registration": "https://forms.gle/sEGW2djeqhXsmd9J8",
+  "instagram"   : "https://www.instagram.com/hacktheridge/",
+  "email"       : "hacktheridge24@gmail.com"
 }
 export default links;
